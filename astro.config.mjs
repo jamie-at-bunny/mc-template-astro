808d1b31f813a82903750ef20e106414c2654b2f
jamie-at-bunny <jamie@bunny.net>
2026-02-18 09:53:50 +0000

    fix: allow bunny.run

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,18 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: process.env.SITE_URL || "http://localhost:4321",
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
+  security: {
+    allowedDomains: [
+      {
+        hostname: "**.bunny.run",
+        protocol: "https",
+      },
+    ],
+  },
   integrations: [mdx(), sitemap()],
 });
